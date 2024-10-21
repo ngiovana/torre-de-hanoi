b0b4fbd19a756a040e176e4942350c5ba8a8bd03
Jezreel Kintino de Moraes <jezreel.moraes@asaas.com.br>
2024-10-21 13:36:04 -0300

    feat: Criando tower

---
+++ javascript/Tower.js
@@ -0,0 +1,39 @@
+import {Disk} from "./Disk.js";
+
+class Tower {
+
+    name
+
+    /**
+     * @type {Array<Disk>}
+     */
+    diskStack;
+
+    constructor(name, diskStack) {
+        this.name = name
+        this.diskStack = diskStack;
+    }
+
+    getDiskStack = () => {
+        return [...this.diskStack]
+    };
+
+    getTopDisk = () => {
+        return this.diskStack[this.diskStack.length - 1]
+    };
+
+    addDisk = (disk) => {
+        this.diskStack.push(disk);
+    };
+
+    removeTopDisk = () => {
+        this.diskStack.pop();
+    };
+
+    size = () => {
+        return this.diskStack.length
+    };
+
+}
+
+export {Tower}
