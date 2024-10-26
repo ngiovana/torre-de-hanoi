c00a26de272f31df53200638fc85b851b8e92be9
Jezreel Kintino de Moraes <jezreel.moraes@asaas.com.br>
2024-10-25 22:34:57 -0300

    feat: Criando soundcontroller

---
+++ javascript/SoundController.js
@@ -0,0 +1,34 @@
+class SoundController {
+
+    playInvalidMoveSound = () => {
+        const audio = new Audio(`assets/audios/invalid.wav`);
+        audio.play();
+    }
+
+    playDragSound = () => {
+        const audio = new Audio(`assets/audios/drag.wav`);
+        audio.play();
+    }
+
+    playMoveSound = () => {
+        const audioNumber = Math.floor(Math.random() * 3) + 1;
+        const audio = new Audio(`assets/audios/move${ audioNumber }.wav`);
+
+        audio.play();
+    }
+
+    playWinSound = (isBestWin) => {
+        if (isBestWin) {
+            const bestWinAudio = new Audio(`assets/audios/best-win.mp3`);
+            bestWinAudio.volume = .09;
+            bestWinAudio.play();
+        }
+
+        const winAudio = new Audio(`assets/audios/win.mp3`);
+        winAudio.volume = .09;
+        winAudio.play();
+    }
+
+}
+
+export {SoundController}
