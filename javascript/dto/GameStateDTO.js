5a20757984f791ccfc5d7aecb70b127a4836aa93
Jezreel Kintino de Moraes <jezreel.moraes@asaas.com.br>
2024-10-27 15:28:06 -0300

    feat: Criando GameState

---
+++ javascript/dto/GameStateDTO.js
@@ -0,0 +1,28 @@
+import {TowerName} from "../enum/TowerName";
+
+class GameStateDTO {
+
+    id;
+    movesCount;
+    isFinished;
+
+    towerDisks;
+    firstTower;
+    middleTower;
+    lastTower;
+
+    constructor(id, towerDisks) {
+        this.id = id;
+
+        this.movesCount = 0;
+        this.isFinished = false;
+
+        this.towerDisks = towerDisks;
+        this.firstTower = towerDisks[TowerName.FIRST_TOWER];
+        this.middleTower = towerDisks[TowerName.MIDDLE_TOWER];
+        this.lastTower = towerDisks[TowerName.LAST_TOWER];
+    }
+
+}
+
+export {GameStateDTO}
