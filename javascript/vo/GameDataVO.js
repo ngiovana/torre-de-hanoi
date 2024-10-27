dbf68dd9ed45e6988db167234dac9b48f3ded190
Jezreel Kintino de Moraes <jezreel.moraes@asaas.com.br>
2024-10-27 15:28:17 -0300

    feat: Criando GameData

---
+++ javascript/vo/GameDataVO.js
@@ -0,0 +1,30 @@
+import {Timer} from "../utils/Timer.js";
+import {HanoiTowerSolver} from "../utils/HanoiTowerSolver.js";
+import {TowerName} from "../enum/TowerName.js";
+
+class GameDataVO {
+
+    id;
+    playerName;
+    difficultLevel;
+    minMoves;
+    state;
+
+    solver;
+    timer;
+
+    constructor(id, playerName, difficultLevel, minMoves, state) {
+        this.id = id
+        this.playerName = playerName
+        this.difficultLevel = difficultLevel
+        this.minMoves = minMoves
+        this.state = state
+
+        this.solver = new HanoiTowerSolver(difficultLevel, ...Object.values(TowerName))
+        this.timer = new Timer();
+    }
+
+
+}
+
+export {GameDataVO}
