8193cf8b5a4bdb61376b44e28da659fa7eab7eef
Jezreel Kintino de Moraes <jezreel.moraes@asaas.com.br>
2024-11-06 00:08:53 -0300

    feat: Criando SierpinskiTriangle

---
+++ javascript/vo/SierpinskiTriangle.js
@@ -0,0 +1,28 @@
+class SierpinskiTriangle {
+
+    nodeMap;
+
+    numDisks;
+
+    iterationNumber;
+
+    gameState;
+
+    left;
+
+    top;
+
+    right;
+
+    triangle;
+
+    countTop;
+
+    countRight;
+
+    countLeft;
+
+}
+
+
+export {SierpinskiTriangle}
