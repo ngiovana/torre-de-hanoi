dcf60e90678fc9682616278341bdd37f741340e9
Jezreel Kintino de Moraes <jezreel.moraes@asaas.com.br>
2024-11-18 18:06:41 -0300

    fix: audio path

--- javascript/game/controller/SoundController.js
+++ javascript/game/controller/SoundController.js
@@ -1,36 +1,36 @@
 class SoundController {
 
     playStarGameSound = () => {
-        const audio = new Audio(`../../assets/audios/start.wav`);
+        const audio = new Audio(`../assets/audios/start.wav`);
         audio.play();
     }
 
     playInvalidMoveSound = () => {
-        const audio = new Audio(`../../assets/audios/invalid.wav`);
+        const audio = new Audio(`../assets/audios/invalid.wav`);
         audio.play();
     }
 
     playDragSound = () => {
-        const audio = new Audio(`../../assets/audios/drag.wav`);
+        const audio = new Audio(`../assets/audios/drag.wav`);
         audio.play();
     }
 
     playMoveSound = () => {
         const moveSoundCount = 3
         const audioNumber = Math.floor(Math.random() * moveSoundCount) + 1;
-        const audio = new Audio(`../../assets/audios/move${ audioNumber }.wav`);
+        const audio = new Audio(`../assets/audios/move${ audioNumber }.wav`);
 
         audio.play();
     }
 
     playWinSound = (isBestWin) => {
         if (isBestWin) {
-            const bestWinAudio = new Audio(`../../assets/audios/best-win.mp3`);
+            const bestWinAudio = new Audio(`../assets/audios/best-win.mp3`);
             bestWinAudio.volume = .09;
             bestWinAudio.play();
         }
 
-        const winAudio = new Audio(`../../assets/audios/win.mp3`);
+        const winAudio = new Audio(`../assets/audios/win.mp3`);
         winAudio.play();
     }
 
